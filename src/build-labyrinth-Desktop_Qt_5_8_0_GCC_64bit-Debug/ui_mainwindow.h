befd27cae3ff08e2fa21d9c7ce5221862fcb79c4
João Leite <carvalhojldc@gmail.com>
2017-03-09 14:40:01 -0300

    .

---
+++ src/build-labyrinth-Desktop_Qt_5_8_0_GCC_64bit-Debug/ui_mainwindow.h
@@ -0,0 +1,188 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.8.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QFormLayout>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QGroupBox *groupBox;
+    QWidget *formLayoutWidget;
+    QFormLayout *formLayout;
+    QLabel *label;
+    QLineEdit *lineEdit;
+    QLabel *label_2;
+    QLineEdit *lineEdit_2;
+    QWidget *formLayoutWidget_2;
+    QFormLayout *formLayout_2;
+    QLabel *label_4;
+    QLineEdit *lineEdit_3;
+    QLabel *label_3;
+    QLineEdit *lineEdit_4;
+    QWidget *gridLayoutWidget;
+    QGridLayout *gridLayout;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_3;
+    QPushButton *pushButton;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(440, 494);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        groupBox = new QGroupBox(centralWidget);
+        groupBox->setObjectName(QStringLiteral("groupBox"));
+        groupBox->setGeometry(QRect(10, 10, 421, 151));
+        formLayoutWidget = new QWidget(groupBox);
+        formLayoutWidget->setObjectName(QStringLiteral("formLayoutWidget"));
+        formLayoutWidget->setGeometry(QRect(10, 30, 151, 61));
+        formLayout = new QFormLayout(formLayoutWidget);
+        formLayout->setSpacing(6);
+        formLayout->setContentsMargins(11, 11, 11, 11);
+        formLayout->setObjectName(QStringLiteral("formLayout"));
+        formLayout->setContentsMargins(0, 0, 0, 0);
+        label = new QLabel(formLayoutWidget);
+        label->setObjectName(QStringLiteral("label"));
+
+        formLayout->setWidget(0, QFormLayout::LabelRole, label);
+
+        lineEdit = new QLineEdit(formLayoutWidget);
+        lineEdit->setObjectName(QStringLiteral("lineEdit"));
+        lineEdit->setEnabled(true);
+        lineEdit->setMaximumSize(QSize(16777215, 233245));
+
+        formLayout->setWidget(0, QFormLayout::FieldRole, lineEdit);
+
+        label_2 = new QLabel(formLayoutWidget);
+        label_2->setObjectName(QStringLiteral("label_2"));
+
+        formLayout->setWidget(1, QFormLayout::LabelRole, label_2);
+
+        lineEdit_2 = new QLineEdit(formLayoutWidget);
+        lineEdit_2->setObjectName(QStringLiteral("lineEdit_2"));
+
+        formLayout->setWidget(1, QFormLayout::FieldRole, lineEdit_2);
+
+        formLayoutWidget_2 = new QWidget(groupBox);
+        formLayoutWidget_2->setObjectName(QStringLiteral("formLayoutWidget_2"));
+        formLayoutWidget_2->setGeometry(QRect(200, 30, 211, 64));
+        formLayout_2 = new QFormLayout(formLayoutWidget_2);
+        formLayout_2->setSpacing(6);
+        formLayout_2->setContentsMargins(11, 11, 11, 11);
+        formLayout_2->setObjectName(QStringLiteral("formLayout_2"));
+        formLayout_2->setContentsMargins(0, 0, 0, 0);
+        label_4 = new QLabel(formLayoutWidget_2);
+        label_4->setObjectName(QStringLiteral("label_4"));
+
+        formLayout_2->setWidget(0, QFormLayout::LabelRole, label_4);
+
+        lineEdit_3 = new QLineEdit(formLayoutWidget_2);
+        lineEdit_3->setObjectName(QStringLiteral("lineEdit_3"));
+
+        formLayout_2->setWidget(0, QFormLayout::FieldRole, lineEdit_3);
+
+        label_3 = new QLabel(formLayoutWidget_2);
+        label_3->setObjectName(QStringLiteral("label_3"));
+
+        formLayout_2->setWidget(1, QFormLayout::LabelRole, label_3);
+
+        lineEdit_4 = new QLineEdit(formLayoutWidget_2);
+        lineEdit_4->setObjectName(QStringLiteral("lineEdit_4"));
+
+        formLayout_2->setWidget(1, QFormLayout::FieldRole, lineEdit_4);
+
+        gridLayoutWidget = new QWidget(groupBox);
+        gridLayoutWidget->setObjectName(QStringLiteral("gridLayoutWidget"));
+        gridLayoutWidget->setGeometry(QRect(120, 100, 291, 31));
+        gridLayout = new QGridLayout(gridLayoutWidget);
+        gridLayout->setSpacing(6);
+        gridLayout->setContentsMargins(11, 11, 11, 11);
+        gridLayout->setObjectName(QStringLiteral("gridLayout"));
+        gridLayout->setContentsMargins(0, 0, 0, 0);
+        pushButton_2 = new QPushButton(gridLayoutWidget);
+        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
+
+        gridLayout->addWidget(pushButton_2, 0, 0, 1, 1);
+
+        pushButton_3 = new QPushButton(gridLayoutWidget);
+        pushButton_3->setObjectName(QStringLiteral("pushButton_3"));
+
+        gridLayout->addWidget(pushButton_3, 0, 1, 1, 1);
+
+        pushButton = new QPushButton(gridLayoutWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+
+        gridLayout->addWidget(pushButton, 0, 2, 1, 1);
+
+        MainWindow->setCentralWidget(centralWidget);
+        groupBox->raise();
+        formLayoutWidget->raise();
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 440, 22));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", Q_NULLPTR));
+        groupBox->setTitle(QApplication::translate("MainWindow", "Configura\303\247\303\243o do labirinto", Q_NULLPTR));
+        label->setText(QApplication::translate("MainWindow", "Linhas", Q_NULLPTR));
+        label_2->setText(QApplication::translate("MainWindow", "Colunas", Q_NULLPTR));
+        label_4->setText(QApplication::translate("MainWindow", "Custo horizontal", Q_NULLPTR));
+        label_3->setText(QApplication::translate("MainWindow", "Custo vertical", Q_NULLPTR));
+        pushButton_2->setText(QApplication::translate("MainWindow", "Exportar", Q_NULLPTR));
+        pushButton_3->setText(QApplication::translate("MainWindow", "Importar", Q_NULLPTR));
+        pushButton->setText(QApplication::translate("MainWindow", "Gerar", Q_NULLPTR));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
